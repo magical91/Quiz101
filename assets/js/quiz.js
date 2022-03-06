a3049925c19b431b7fe050af07140d71a4436270
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-06 15:28:06 -0600

    hid start button and show questions with its corresponding functions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -8,15 +8,21 @@
 
 
 var startButton = document.getElementById("start-btn");
-var questionContentEl = document.getElementById("container")
+var nextButton = document.getElementById("next-btn");
+var questionContentEl = document.getElementById("container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answers")
+
+let shuffleQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame);
 
 
 
 function startGame() {
-    console.log("started");
     startButton.classList.add("hide");
+    shuffleQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContentEl.classList.remove("hide");
     nextQuestion();
 }
@@ -26,13 +32,69 @@ function startGame() {
 
 
 function nextQuestion() {
+    resetState();
+    showQuestion(shuffleQuestions[currentQuestionIndex]);
+}
 
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerButtonsEl.appendChild(button);
+    });
+}
+
+function resetState() {
+    nextButton.classList.add("hide");
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
+
+function selectAnswer(e) {
+    var selectedButton = e.target;
+    var correct = selectedButton.dataset.correct;
+    seStatusClass(document.body, correct);
+    Array.from(answerButtonsEl.children).forEach(button => {
+        seStatusClass(button, button.dataset.correct)
+    })
+};
+
+
+function seStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+};
+
+
+function clearStatusClass(element) {
+    element.classList.remove("remove")
+    element.classList.remove("wrong")
 }
 
 
 
 
-
-function selectAnswer() {
-
-}
+var questions = [
+    {
+        question: "Which of the provided answers is NOT a data type?",
+        answers: [
+            { text: "Boolean", correct: false },
+            { text: "Object", correct: false},
+            { text: "Function", correct: true },
+            { text: "Symbol", correct: false }
+        ]
+    }
+]
